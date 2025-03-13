62b78b3781188a3de632aa007675a6331a129877
DavidIESBM <david.fonsceca@iesb.edu.br>
2025-03-13 20:21:54 -0300

    testes

---
+++ LogicaDeProgramacao/teste1.js
@@ -0,0 +1,23 @@
+const entrada = require('prompt-sync') ({sigint: true});
+console.log("-------------------------------");
+var n1 = parseInt(entrada("Digite um numero: "));
+console.log("-------------------------------");
+var n2 = parseInt(entrada("Digite outro número: "));
+var soma = n1 + n2;
+var subtrai = n1 - n2
+var mutiplica = n1 * n2;
+var divide = n1 / n2
+
+
+console.log("-------------------------------");
+console.log(`A soma de ${n1} + ${n2} é igual a: ${soma}`)
+console.log("-------------------------------");
+console.log("-------------------------------");
+console.log(`A soma de ${n1} + ${n2} é igual a: ${subtrai}`)
+console.log("-------------------------------");
+console.log("-------------------------------");
+console.log(`A soma de ${n1} + ${n2} é igual a: ${divide}`)
+console.log("-------------------------------");
+console.log("-------------------------------");
+console.log(`A soma de ${n1} + ${n2} é igual a: ${mutiplica}`)
+console.log("-------------------------------");
